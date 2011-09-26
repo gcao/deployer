7ec7c599e865b9f8b419f724bf2d1edf68b14e53
Guoliang Cao <gcao99@gmail.com>
2011-09-26 09:41:01 -0400

    Fix solo configuration

--- solo.rb
+++ solo.rb
@@ -1,3 +1,3 @@
-cookbook_path     %W"#{File.dirname(__FILE__)}/cookbooks #{File.dirname(__FILE__)}/my_cookbooks}"
+cookbook_path     %W"#{File.dirname(__FILE__)}/cookbooks #{File.dirname(__FILE__)}/my-cookbooks"
 file_cache_path   File.dirname(__FILE__)
 log_level         :debug
